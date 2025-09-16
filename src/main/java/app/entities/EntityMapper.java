b2825bf533891d2c54565ccda26a3f296e7a011f
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-09-16 09:16:36 +0200

    Implemented entityMapper, just a draft need dtos implemented

---
+++ src/main/java/app/entities/EntityMapper.java
@@ -0,0 +1,52 @@
+package app.entities;
+
+import java.util.List;
+
+public class EntityMapper {
+    public List<Actor> convertToActor(List<Dto> dtoList) {
+        return dtoList.stream()
+                .map(dto -> {
+                    Actor actor = new Actor();
+                    actor.setId(dto.getId());
+                    actor.setName(dto.getName());
+                    return actor;
+                })
+                .toList();
+    }
+
+    public List<Director> convertToDirector(List<Dto> dtoList) {
+        return dtoList.stream()
+                .map(dto -> {
+                    Director director = new Director();
+                    director.setId(dto.getId());
+                    director.setName(dto.getName());
+                    return director;
+                })
+                .toList();
+    }
+
+    public List<Genre> convertToGenre(List<Dto> dtoList) {
+        return dtoList.stream()
+                .map(dto -> {
+                    Genre genre = new Genre();
+                    genre.setId(dto.getGenreId());
+                    genre.setName(dto.getGenreName());
+                    return genre;
+                })
+                .toList();
+    }
+
+    public List<Movie> convertToMovie(List<Dto> dtoList) {
+        return dtoList.stream()
+                .map(dto -> {
+                    Movie movie = new Movie();
+                    movie.setId(dto.getMovieId());
+                    movie.setTitle(dto.getMovieTitle());
+                    movie.setReleaseDate(dto.getReleaseDate());
+                    movie.setRating(dto.getRating());
+                    movie.setPopularity(dto.getPopularity());
+                    return movie;
+                })
+                .toList();
+    }
+}
